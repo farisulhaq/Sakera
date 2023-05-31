89f4b7cec9eae9bfc93d3c1dd749e38bcab3c573
farisulhaq <faries089@gmail.com>
2023-05-31 23:59:00 +0700

    modif rencana

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -91,7 +91,7 @@
                   </div>
                   <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                     <h6 class="text-muted font-semibold">Total Anggaran Seksi {{ Str::ucfirst($user->name) }}</h6>
-                    <h6 class="font-extrabold mb-0">{{ 'Rp. ' . number_format($user->roles[0]->kegiatans->sum('pagu'), 0, ',', '.') }}</h6>
+                    <h6 class="font-extrabold mb-0">@rupiah($user->roles[0]->kegiatans->sum('pagu'))</h6>
                   </div>
                 </div>
               </div>
